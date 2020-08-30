be97037572d6953cca2e4e309c39f1681c8f003a
norimoji <zimmerzing@gmail.com>
2020-08-30 20:45:50 +0100

    Add files via upload

---
+++ commands/soundboard/baka.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class meat extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'baka',
+			group: 'soundboard collection',
+			memberName: 'baka',
+			description: 'Bot say BAKA!.',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/Baka.mp3')
+    }
+};
