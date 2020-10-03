bf811fd5b29c8e25acd03896af05aef6c3868fa5
norimoji <zimmerzing@gmail.com>
2020-10-03 23:36:36 +0100

    Add files via upload

---
+++ commands/soundboard/sounds/notenough.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class meat extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'notenough',
+			group: 'soundboard collection',
+			memberName: 'notenough',
+			description: 'Bot say I dont think you have the facilities for that big man.',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/notenough.mp3')
+    }
+};
