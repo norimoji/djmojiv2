f3fcedd3d16768671ed3ce090a3fd6ddcd687b52
norimoji <zimmerzing@gmail.com>
2020-10-04 00:50:41 +0100

    Add files via upload

---
+++ commands/soundboard/wii.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class meat extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'wii',
+			group: 'soundboard collection',
+			memberName: 'wii',
+			description: 'Bot plays the wii theme.',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/wii.mp3')
+    }
+};
