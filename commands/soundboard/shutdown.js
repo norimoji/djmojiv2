fe989bf58fc09f9109ecb915f69a29e2a844aa70
norimoji <zimmerzing@gmail.com>
2020-10-03 15:03:42 +0100

    Add files via upload

---
+++ commands/soundboard/shutdown.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class meat extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'shutdown',
+			group: 'soundboard collection',
+			memberName: 'shutdown',
+			description: 'Bot say Shutdown!.',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/shutdown.mp3')
+    }
+};
