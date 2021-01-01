2d169d02bce6cba40b7b7c764ce35263886d8d95
norimoji <zimmerzing@gmail.com>
2021-01-01 20:23:59 +0000

    Add files via upload

---
+++ commands/soundboard/nogod.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class meat extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'nogod',
+			group: 'soundboard collection',
+			memberName: 'nogod',
+			description: 'Bot say no god no!',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/nogod.mp3')
+    }
+};
