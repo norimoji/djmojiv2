a8fe1e99f0b6f94d08fef707102214f764f489b2
norimoji <zimmerzing@gmail.com>
2021-01-11 22:22:25 +0000

    Add files via upload

---
+++ commands/soundboard/intro.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class meat extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'intro',
+			group: 'soundboard collection',
+			memberName: 'intro',
+			description: 'Bot play Attack on Titan S1 opening song',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/intro.mp3')
+    }
+};
