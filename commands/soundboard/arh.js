742e2085fb9a82811a280d47b2706704c672ff9c
norimoji <zimmerzing@gmail.com>
2021-01-13 22:38:30 +0000

    Add files via upload

---
+++ commands/soundboard/arh.js
@@ -0,0 +1,19 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class meat extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'arh',
+			group: 'soundboard collection',
+			memberName: '30',
+			description: 'Bot say arhhhhhhhhhhhhhh',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+		playSound.run(message,'./sounds/arh.mp3')
+		console.log(message)
+    }
+};
