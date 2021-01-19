941d6d7f0f8f9e5c33300084b35aa84830305f48
norimoji <zimmerzing@gmail.com>
2021-01-19 20:27:34 +0000

    Update 10.js

--- commands/soundboard/10.js
+++ commands/soundboard/10.js
@@ -1,7 +1,7 @@
 const { Command } = require('discord.js-commando');
 const soundboard = require('./soundboardMaster');
 
-module.exports = class 10 extends Command {
+module.exports = class ten extends Command {
 	constructor(client) {
 		super(client, {
 			name: '10',
