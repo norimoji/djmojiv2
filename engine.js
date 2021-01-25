0be6c5ee480e38a08cd0d4a1e205443007d5e8a9
norimoji <zimmerzing@gmail.com>
2021-01-25 17:57:23 +0000

    Add files via upload

--- engine.js
+++ engine.js
@@ -5,7 +5,6 @@ const { sup } = require('ffmpeg-static');
 const path = require('path');
 const soundboard = require('./commands/soundboard/soundboardMaster');
 const findUsers = require('./commands/BotUtil/findUsersInVoiceChannel');
-const leaveVoiceChannelCommand = require('./commands/BotUtil/leaveVoiceChannel');
 
 const client = new CommandoClient({
 	commandPrefix: process.env.PREFIX,
@@ -43,22 +42,22 @@ client.once('ready', () => {
 });
 
 client.on('voiceStateUpdate', (oldVoiceState, newVoiceState) => {
-// 	console.log(newVoiceState.selfDeaf)
-// 	console.log(newVoiceState.mute)
+
 	if (newVoiceState.channel) { // The member connected to a channel. 
-		if(newVoiceState.mute != true && newVoiceState.deaf != true){
-// 			console.log(`newMember size ` + newVoiceState.channel.members.size)
+		if(newVoiceState.selfMute == false && newVoiceState.selfDeaf == false){
+			if(oldVoiceState.selfMute == false && oldVoiceState.selfDeaf == false){
 				if(newVoiceState.channel.members.size >=2){
 					var playSound = new soundboard(client)
 					playSound.run(newVoiceState,'./sounds/ohyeah.mp3')
 				}
+			}
 		}	
 	} else if (oldVoiceState.channel) { // The member disconnected from a channel.
 		if(oldVoiceState.channel.members.size = 1 && oldVoiceState.channel.members.has(`561896933879185431`)){
 			const activeVoiceChannel = availableServer.getMemberVoiceLocation(newVoiceState.guild.name)
 			availableServer.getBotToLeave(newVoiceState,activeVoiceChannel)	
 		}
-		console.log(`oldMember size ` + oldVoiceState.channel.members.size)}
+	}
 })
 
 client.on('error', console.error);
