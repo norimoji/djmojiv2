ec848ad8526075d6eb65e9b451ee63985d8e3071
norimoji <zimmerzing@gmail.com>
2021-02-15 22:52:33 +0000

    Add files via upload

---
+++ commands/soundboard/fuck.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class fuck extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'fuck',
+			group: 'soundboard collection',
+			memberName: 'fuck',
+			description: 'Bot say Ah fuck, I can't believe you done this.',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/fuck.mp3')
+    }
+};
