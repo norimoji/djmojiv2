a00d606c5cb514e18ad0dc7930a30ccf2e1f450a
norimoji <zimmerzing@gmail.com>
2021-02-25 18:06:23 +0000

    Update engine.js

--- engine.js
+++ engine.js
@@ -3,7 +3,7 @@ const { VoiceChannel, DiscordAPIError, MessageCollector, MessageFlags, Message }
 const { CommandoClient } = require('discord.js-commando');
 const { sup } = require('ffmpeg-static');
 const path = require('path');
-const soundboard = require('./commands/soundboard/soundboardMaster');
+const soundboard = require('./commands/soundboard/shutdown');
 const findUsers = require('./commands/BotUtil/findUsersInVoiceChannel');
 const mongo = require('./mongo')
 const setupIntro = require('./commands/BotUtil/setupIntro');
