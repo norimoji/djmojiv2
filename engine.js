8094fdbc8e91cda81997d7176f7a0d14010115f9
norimoji <zimmerzing@gmail.com>
2021-02-27 16:34:55 +0000

    Update engine.js

--- engine.js
+++ engine.js
@@ -66,8 +66,6 @@ client.on('voiceStateUpdate', (oldVoiceState, newVoiceState) => {
 			}else if(newVoiceState.channel.members.size >=2){
 				var playSound = new soundboard(client)
 				playSound.run(newVoiceState,'./sounds/'+ setup.getUserIntro(newVoiceState.guild.id, newVoiceState.member.id) +'.mp3')
-				console.log('iam here')
-				console.log(setup.getUserIntro(newVoiceState.guild.id, newVoiceState.member.id))
 			}
 		}
 	} else if (oldVoiceState.channel) { // The member disconnected from a channel.
