97c79ea8a968e7ad7cb3bc23f36fbe755ec64573
norimoji <zimmerzing@gmail.com>
2021-04-05 00:40:28 +0100

    Add files via upload

---
+++ commands/soundboard/fart1.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class fart1 extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'fart1',
+			group: 'soundboard collection',
+			memberName: 'fart1',
+			description: 'Bot fart1',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/fart1.mp3')
+    }
+};
