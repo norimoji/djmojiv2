687e04a4956c005734a6f3fbd0440277b83cfafb
norimoji <zimmerzing@gmail.com>
2021-07-04 20:21:13 +0100

    Add files via upload

---
+++ commands/reactions/think.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord.js-commando');
+const reaction = require('./reaction');
+const fs = require('fs')
+const path = require('path')
+
+
+module.exports = class think extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'think',
+			group: 'reaction command',
+			memberName: 'think',
+			description: 'Think meme from Invisible.',
+		});
+	}
+
+	run(message){
+		const think = new reaction(this.client)
+        think.run(message,'./commands/reactions/expressions/think.png')
+		message.send('Think, ' + message.content.substring(7) + ' Think!')
+    }
+};
