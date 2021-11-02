030ce8b7f1134869264c118cf7f6e4635c9f9419
norimoji <zimmerzing@gmail.com>
2021-11-02 22:12:07 +0000

    Add files via upload

---
+++ commands/soundboard/wae.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class wae extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'wae',
+			group: 'soundboard collection',
+			memberName: 'wae',
+			description: 'Bot say do you know da wae?',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/dawae.mp3')
+    }
+};
