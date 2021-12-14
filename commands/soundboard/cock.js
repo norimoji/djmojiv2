d70cae4ec5f6ac0a5343211940301464094c510d
norimoji <zimmerzing@gmail.com>
2021-12-14 23:16:46 +0000

    Add files via upload

---
+++ commands/soundboard/cock.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class and extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'cock',
+			group: 'soundboard collection',
+			memberName: 'cock',
+			description: 'Bot say nice cock.',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/cock.mp3')
+    }
+};
