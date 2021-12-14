56f83df6adfed8ce233236c650a4f41cefa99f06
norimoji <zimmerzing@gmail.com>
2021-12-14 23:17:59 +0000

    Update cock.js

--- commands/soundboard/cock.js
+++ commands/soundboard/cock.js
@@ -1,7 +1,7 @@
 const { Command } = require('discord.js-commando');
 const soundboard = require('./soundboardMaster');
 
-module.exports = class and extends Command {
+module.exports = class cock extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'cock',
