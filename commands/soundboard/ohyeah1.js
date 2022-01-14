834e20552e5056c4fb8593362a09f01f803dd0a1
norimoji <zimmerzing@gmail.com>
2022-01-14 23:50:57 +0000

    Add files via upload

--- commands/soundboard/ohyeah1.js
+++ commands/soundboard/ohyeah1.js
@@ -1,18 +1,18 @@
 const { Command } = require('discord.js-commando');
 const soundboard = require('./soundboardMaster');
 
-module.exports = class intro4 extends Command {
+module.exports = class ohyeah1 extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'intro4',
+			name: 'ohyeah1',
 			group: 'soundboard collection',
-			memberName: 'intro4',
-			description: 'Bot plays Teriyaki Boy Tokyo Drift.',
+			memberName: 'ohyeah1',
+			description: 'Bot say oh yeah (sexually).',
 		});
 	}
 
 	async run(message){
 		var playSound = new soundboard(this.client)
-        playSound.run(message,'./sounds/intro4.mp3')
+        playSound.run(message,'./sounds/ohyeah1.mp3')
     }
 };
--- commands/soundboard/yeah1.js
+++ commands/soundboard/yeah1.js
@@ -1,18 +1,18 @@
 const { Command } = require('discord.js-commando');
 const soundboard = require('./soundboardMaster');
 
-module.exports = class intro4 extends Command {
+module.exports = class yeah1 extends Command {
 	constructor(client) {
 		super(client, {
-			name: 'intro4',
+			name: 'yeah1',
 			group: 'soundboard collection',
-			memberName: 'intro4',
-			description: 'Bot plays Teriyaki Boy Tokyo Drift.',
+			memberName: 'yeah1',
+			description: 'Bot say yeah (deeply).',
 		});
 	}
 
 	async run(message){
 		var playSound = new soundboard(this.client)
-        playSound.run(message,'./sounds/intro4.mp3')
+        playSound.run(message,'./sounds/yeah1.mp3')
     }
 };
