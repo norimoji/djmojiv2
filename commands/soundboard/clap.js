2831d4dbd2bfb62331f95ccb737822859e6cdf73
norimoji <zimmerzing@gmail.com>
2022-01-23 23:25:47 +0000

    Add files via upload

---
+++ commands/soundboard/clap.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class clap extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'clap',
+			group: 'soundboard collection',
+			memberName: 'clap',
+			description: 'Bot say clapping sound.',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/clap.mp3')
+    }
+};
---
+++ commands/soundboard/clap1.js
@@ -0,0 +1,18 @@
+const { Command } = require('discord.js-commando');
+const soundboard = require('./soundboardMaster');
+
+module.exports = class clap1 extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'clap1',
+			group: 'soundboard collection',
+			memberName: 'clap1',
+			description: 'Bot say clapping sound.',
+		});
+	}
+
+	async run(message){
+		var playSound = new soundboard(this.client)
+        playSound.run(message,'./sounds/clap1.mp3')
+    }
+};
